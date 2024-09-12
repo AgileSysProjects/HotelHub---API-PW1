0d6f112d644a3251cf7c490cd8cc1dc20fb91e69
Guilherme Henrique <guilherme.pessoa@academico.ifpb.edu.br>
2024-09-12 15:02:24 -0300

    Criando rotas para quarto

--- src/routers/index.routes.ts
+++ src/routers/index.routes.ts
@@ -2,12 +2,12 @@ import { Application } from "express";
 
 import userRoutes from "./usuario.routes";
 import hotelRoutes from "./hotel.routes";
+import quartoRoutes from "./quarto.routes";
 
 const routes = (app:Application) => {
     userRoutes(app);
     hotelRoutes(app);
+    quartoRoutes(app)
 }
 
 export default routes;
-
-
